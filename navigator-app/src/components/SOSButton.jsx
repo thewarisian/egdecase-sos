eb2371609e0abdcedcb122ca4594546e09b739be
Ayaan Waris <aianvarys@Ayaans-MacBook-Air.local>
2026-04-16 09:19:09 +0530

    Added placeholder white flash on screen when SOS Button clicked

--- navigator-app/src/components/SOSButton.jsx
+++ navigator-app/src/components/SOSButton.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
+import SOSPanel from "./SOSPanel"
+
 function SOSButton() {
+    const [flash, setFlash] = useState(false)
+
+    const flashPanel = () => {
+        setFlash(true)
+
+        setTimeout(
+            () => {
+                setFlash(false)
+            }, 500
+        )
+    }
+
     return (
+        <>
+        {/* Show Panel when clicked */}
+        <SOSPanel flash={flash}></SOSPanel>
+        
+        {/* SOS Button Structure */}
         <button
+        onClick={flashPanel}
         className="fixed bottom-0 left-0 w-full 
                     h-16 bg-red-700 text-xl
                     text-white font-bold text-base rounded-none">
             I NEED CALM NOW
         </button>
+        </>
     )
 }
 
---
+++ navigator-app/src/components/SOSPanel.jsx
@@ -0,0 +1,9 @@
+function SOSPanel({flash}) {
+    if (!flash) return null
+
+    return (
+        <div className="fixed inset-0 bg-white z-50"></div>
+    )
+}
+
+export default SOSPanel
