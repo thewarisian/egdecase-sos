a9127cd007c2c8ce3a55c80cc6411b91034e58f3
Ayaan Waris <aianvarys@Ayaans-MacBook-Air.local>
2026-04-16 08:51:26 +0530

    Added Red SOS Button attached to bottom of window panel

--- navigator-app/src/App.jsx
+++ navigator-app/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+/* SOS Feature Inputs */
+import SOSButton from './components/SOSButton'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -114,6 +117,9 @@ function App() {
 
       <div className="ticks"></div>
       <section id="spacer"></section>
+
+      {/* SOS Feature Blocks added here */}
+      <SOSButton />
     </>
   )
 }
---
+++ navigator-app/src/components/SOSButton.jsx
@@ -0,0 +1,12 @@
+function SOSButton() {
+    return (
+        <button
+        className="fixed bottom-0 left-0 w-full 
+                    h-16 bg-red-700 text-xl
+                    text-white font-bold text-base rounded-none">
+            I NEED CALM NOW
+        </button>
+    )
+}
+
+export default SOSButton
